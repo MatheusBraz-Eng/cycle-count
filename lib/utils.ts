58ffc2f5a56b88826c8252acaa2d711cc21f3312
MatheusBraz-Eng <matheusgamabrazsm@gmail.com>
2026-03-21 20:31:56 -0300

    nova estrutura do projeto

---
+++ lib/utils.ts
@@ -0,0 +1,11 @@
+import { InventoryUpload } from "@/types";
+
+export const mockUploads: InventoryUpload[] = [
+  {
+    id: "1",
+    uploaded_by: "user-1",
+    file_name: "estoque.xlsx",
+    created_at: new Date().toISOString(),
+    upload_type: "inventory",
+  },
+];
